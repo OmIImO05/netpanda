ad68920869bf4f346497f7259f1c89d1930d2a6b
Shabiha Hossain <sabihahossain4105@gmail.com>
2024-02-20 14:59:49 -0500

    adding things

--- docs/index.md
+++ docs/index.md
@@ -13,4 +13,11 @@
 
 ## Features
 
--   TODO
+-   To detect the lat and long values of the cities of the TN.
+
+## Demo code
+For the demonstration you can visit- 
+"[![image](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/OmIImO05/netpanda/blob/main/docs/examples/example._code.ipynb)"
+
+## Logo
+![netpanda](C:\Users\Shabiha\netpanda\netpanda edit.JPG)
--- netpanda/common.py
+++ netpanda/common.py
@@ -5,3 +5,27 @@ def hello_world():
     """Prints "Hello World!" to the console.
     """
     print("Hello World!")
+
+
+from geopy.geocoders import Nominatim
+
+def get_coordinates(city, state):
+    geolocator = Nominatim(user_agent="my_geocoder")
+    location = geolocator.geocode(f"{city}, {state}")
+    if location:
+        return location.latitude, location.longitude
+    else:
+        return None
+
+# Example usage for cities in Tennessee
+cities_in_tennessee = ["Nashville", "Memphis", "Knoxville", "Chattanooga", "Clarksville"]
+
+for city in cities_in_tennessee:
+    coordinates = get_coordinates(city, "Tennessee")
+    
+    if coordinates:
+        lat, lon = coordinates
+        print(f"{city}: Latitude {lat}, Longitude {lon}")
+    else:
+        print(f"Could not retrieve coordinates for {city}")
+
