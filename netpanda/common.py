e62f917e3cce3865529c3bf6963d88756889dc96
Shabiha Hossain <sabihahossain4105@gmail.com>
2024-02-21 22:29:55 -0500

    edit2common

--- netpanda/common.py
+++ netpanda/common.py
@@ -7,39 +7,11 @@ def hello_world():
     print("Hello World!")
 
 
-# city_to_state.py
-def get_state_for_city(city):
-    us_states = {
-        "New York": "New York",
-        "Los Angeles": "California",
-        "Chicago": "Illinois",
-        "Houston": "Texas",
-        "Phoenix": "Arizona",
-        "Nashville": "Tennessee",
-        "Little Rock": "Arkansas",
-        "Oklahoma City": "Oklahoma",
-        "Indianapolis": "Indiana",
-        "Columbus": "Ohio",
-        "Denver": "Colorado"
+import random
 
-    }
-
-    return us_states.get(city, "Unknown")
-
-def main():
-    # Ask the user to input a city name
-    city_name = input("Enter a city name: ")
-
-    # Get the corresponding state
-    state_name = get_state_for_city(city_name)
-
-    # Display the result
-    if state_name != "Unknown":
-        print(f"The city of {city_name} is in the state of {state_name}.")
-    else:
-        print(f"Sorry, we don't have information for the city of {city_name}.")
-
-if __name__ == "__main__":
-    main()
+def random_number_in_range(start, end):
+    return random.randint(start, end)
+result = random_number_in_range(1, 10)
+print(result)
 
 
