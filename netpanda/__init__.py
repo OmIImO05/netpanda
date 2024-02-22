e4a5d691acbbde133f5f039842af0735e55c5a20
Shabiha Hossain <sabihahossain4105@gmail.com>
2024-02-21 23:29:25 -0500

    init_edit2

--- netpanda/__init__.py
+++ netpanda/__init__.py
@@ -4,4 +4,5 @@ __author__ = """Shabiha Hossain"""
 __email__ = "sabihahossain4105@gmail.com"
 __version__ = "0.0.1"
 
+print("Initializing netpanda package")
 from .common import get_and_print_city_state
