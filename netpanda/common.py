f3e1796e6bfe4274465258bc00244ac64d4aa630
Shabiha Hossain <sabihahossain4105@gmail.com>
2024-02-21 22:54:08 -0500

    edit4_common

--- netpanda/common.py
+++ netpanda/common.py
@@ -15,6 +15,7 @@ result = random_number_in_range(1, 10)
 print(result)
 
 
+
 def get_state_for_city(city):
     us_states = {
         "New York": "New York",
@@ -22,25 +23,18 @@ def get_state_for_city(city):
         "Chicago": "Illinois",
         "Houston": "Texas",
         "Phoenix": "Arizona",
-        # Add more city-state mappings as needed
+        "Nashville": "Tennessee",
+        "Denver": "Colorado"
     }
 
     return us_states.get(city, "Unknown")
 
-def main():
-    # Ask the user to input a city name
-    city_name = input("Enter a city name: ")
 
-    # Get the corresponding state
+def get_and_print_city_state():
+    city_name = input("Enter a city name: ")
     state_name = get_state_for_city(city_name)
 
-    # Display the result
     if state_name != "Unknown":
         print(f"The city of {city_name} is in the state of {state_name}.")
     else:
         print(f"Sorry, we don't have information for the city of {city_name}.")
-
-if __name__ == "__main__":
-    main()
-
-
