26cc28c69e77e33a79f522dd85d7a9544b1a3349
Shabiha Hossain <sabihahossain4105@gmail.com>
2024-03-28 02:43:57 -0400

    commit

--- netpanda/netpanda.py
+++ netpanda/netpanda.py
@@ -5,7 +5,7 @@ from ipyleaflet import basemaps
 
 
 class Map(ipyleaflet.Map):
-    
+
     def __init__(self, center=[20, 0], zoom=2, **kwargs):
         super().__init__(center=center, zoom=zoom, **kwargs)
 
